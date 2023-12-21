9dd53bcfcd2d672a73bac8cf79936d0d8c73f409
Andrew Rueda <andrewrueda@Andrews-Macbook-Pro.local>
2023-12-21 18:02:57 -0500

    add shell script

--- run_negotiate.sh
+++ run_negotiate.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-file_path="/.ollama/models/manifests/registry.ollama.ai/library/llama2/latest"
+homedir=~
+eval homedir=$homedir
+file_path="$homedir/.ollama/models/manifests/registry.ollama.ai/library/llama2/latest"
 
 # Check if file exists
 if [ -e "$file_path" ]; then
@@ -8,13 +10,16 @@ if [ -e "$file_path" ]; then
 else
   echo "llama2 not on computer. Downloading..."
 
-  command="ollama pull llama2"
-  $command
+  ollama_command="ollama pull llama2"
+  $ollama_command
 
   if [ $? -eq 0 ]; then
     echo "Download successful!"
   else
     echo "Download failed. Makde sure you have Ollama installed."
     exit 1
-  fi
+    fi
 fi
+
+streamlit_command="streamlit run main.py"
+$streamlit_command
